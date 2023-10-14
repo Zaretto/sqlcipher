9abcb262deed68ecbc6e53381c436d512dbf1f9f
Richard Harrison <rjh@zaretto.com>
2023-10-14 14:43:28 +0100

    RJH: added build scripts and readme

---
+++ bld/bld.bat
@@ -0,0 +1,5 @@
+nmake /f ..\Makefile.msc TOP=.. clean
+nmake /f ..\Makefile.msc TOP=..
+copy/y sqlite3.c    I:\dev\ark\chemdata\ver14\ll_api\SQLite\SQLite3Crypt\sqlite3crypt\
+copy/y sqlite3.h    I:\dev\ark\chemdata\ver14\ll_api\SQLite\SQLite3Crypt\sqlite3crypt\
+copy/y sqlite3ext.h I:\dev\ark\chemdata\ver14\ll_api\SQLite\SQLite3Crypt\sqlite3crypt\
